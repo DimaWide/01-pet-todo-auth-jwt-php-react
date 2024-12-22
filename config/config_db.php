5b4a35fe586988754f6d67a4abaf9e49082613d1
DmitriyChiroky <dwchirocky@gmail.com>
2024-12-23 01:17:17 +0200

    off

--- config/config_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'DB_HOST' => $_ENV['DB_HOST'],
-    'DB_NAME' => $_ENV['DB_NAME'],
-    'DB_USER' => $_ENV['DB_USER'],
-    'DB_PASSWORD' => $_ENV['DB_PASSWORD'],
-    'DB_PORT' => $_ENV['DB_PORT'],
-];
